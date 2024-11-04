ce7bff3ad4e986d618ce010a201b5584e4f66acb
weibingwen-txair <pacnake.wbw@gamil.com>
2024-11-04 22:45:59 +0800

    get the initial code

---
+++ bingwen/robocasa_demo_test.sh
@@ -0,0 +1,10 @@
+python -m mani_skill.examples.demo_random_action \
+  -e "RoboCasaKitchen-v1" \
+  -n 4 -s 0 1 2 3 \
+  --render-mode="human" -r "fetch"
+
+
+# python -m mani_skill.bingwen.robocasa_demo \
+#   -e "RoboCasaCustomKitchen" \
+#   -n 4 -s 0 1 2 3 \
+#   --render-mode="human" -r "fetch"
